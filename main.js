1432010f4580cc3c6b1b630ce49fa78fcd5d34a0
BornCreator <73579033+BornCreator@users.noreply.github.com>
2021-01-17 15:03:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
+var a='';
 function setup(){
-    canvas=createCanvas(450,400);
-    canvas.center();
+    b=createCanvas(480,400);
+    b.center();
+    c=createCapture(VIDEO);
+    c.size(480,400);
+    c.hide();
+}
+function draw(){
+    image(c,0,0,480,400);
+}
+function start(){
+    d=ml5.objectDetector('cocossd',modelloaded);
+    document.getElementById('status').innerHTML='Status: Detecting Objects';
+    e=document.getElementById('objectName').value;
+}
+function modelloaded(){
+    console.log('Model has Been Loaded');
+    a=true;
 }
